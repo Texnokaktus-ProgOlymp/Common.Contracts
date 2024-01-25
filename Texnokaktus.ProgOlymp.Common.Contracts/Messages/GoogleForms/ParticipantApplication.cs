e0a687e3cb52c5f238dc1538182ac36435377044
Artem Krylov <kav128@outlook.com>
2024-01-25 12:34:44 +0300

    update ParticipantApplication contract

--- Texnokaktus.ProgOlymp.Common.Contracts/Messages/GoogleForms/ParticipantApplication.cs
+++ Texnokaktus.ProgOlymp.Common.Contracts/Messages/GoogleForms/ParticipantApplication.cs
@@ -4,16 +4,17 @@ public record ParticipantApplication
 {
     public required int ContestStageId { get; init; }
     public required DateTime SubmittedTime { get; init; }
-    public required string ContestLocation { get; init; }
-    public required string YandexIdLogin { get; init; }
+    public required string AgeCategory { get; init; }
     public required string ParticipantName { get; init; }
     public required DateOnly BirthDate { get; init; }
+    public required string ParticipantSnils { get; set; }
     public required string ParticipantGrade { get; init; }
     public required string ParticipantEmail { get; init; }
+    public required bool ParticipantEmailConfirm { get; init; }
     public required string School { get; init; }
     public required string SchoolRegion { get; init; }
     public required string ParentName { get; init; }
-    public required string ParentEmail { get; init; }
+    public required string? ParentEmail { get; init; }
     public required string? ParentPhone { get; init; }
     public required bool PersonalDataConsent { get; init; }
     public required string? TeacherName { get; init; }
